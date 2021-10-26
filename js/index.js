372d2b865119986011028845e6baf21fe8baaa3c
CecileAnna <6263325@rts-365.dk>
2021-10-26 10:48:15 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            Accept: "application/json",
+        }
+    })
+    .then(response => {
+        return response.json()
+            .then(jokeData => {
+                console.log(jokeData);
+                const jokearea = document.querySelector("#jokearea");
+                jokearea.textContent = jokeData.joke;
+            })
+    });
